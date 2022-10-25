0fc65674057f02a1ac94963241799e97e8bea71f
Woodensponge <43834352+Woodensponge@users.noreply.github.com>
2022-10-25 16:03:23 -0400

    Embed an AssetBundle

---
+++ uk-mod-hitmarker/AssetHelper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Reflection;
+
+namespace HitmarkerMod
+{
+    public class AssetHelper
+    {
+        static AssetBundle embeddedAssetBundle;
+
+        public static AssetBundle EmbeddedAssetBundle { get => embeddedAssetBundle; }
+
+        public static AssetBundle LoadEmbeddedAssetBundle()
+        {
+            Debug.Log("Hitmarker: Loading Embedded AssetBundle");
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "HitmarkerMod.Resources.AssetBundles.hitmarker";
+
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            if (stream == null)
+            {
+                Debug.LogError("Hitmarker: Cannot load default AssetBundle embedded in the dll.");
+            }
+
+            embeddedAssetBundle = AssetBundle.LoadFromStream(stream);
+            if (embeddedAssetBundle == null)
+            {
+                Debug.LogError("Hitmarker: Obtained Embedded AssetBundle file, but could not create it. Probably already loaded.");
+            }
+
+            return embeddedAssetBundle;
+        }
+    }
+}
---
+++ uk-mod-hitmarker/HitmarkerMod.cs
@@ -0,0 +1,46 @@
+﻿using UMM;
+using UnityEngine;
+//TODO: Move to another file later
+namespace HitmarkerMod
+{
+    [UKPlugin("Hitmarker Mod", "1.0.0", "Adds a customizeable hitmarker to the game. Hitmarkers are both optionally auditory and visual.", true, true)]
+    public class HitmarkerMod : UKMod
+    {
+        public override void OnModLoaded()
+        {
+            //TODO: Move to another file later
+            On.NewMovement.Start += NewMovement_Start;
+            Debug.Log("Hitmarker: you're balls");
+
+            AssetHelper.LoadEmbeddedAssetBundle();
+        }
+
+        private void NewMovement_Start(On.NewMovement.orig_Start orig, NewMovement self)
+        {
+            Debug.Log("Hitmarker: balls sniffer");
+            var assetBundle = AssetHelper.EmbeddedAssetBundle;
+
+            if (assetBundle == null)
+            {
+                Debug.Log("Hitmarker: cock");
+                orig(self);
+                return;
+            }
+
+            var prefab = assetBundle.LoadAsset<GameObject>("MyBalls");
+
+
+            if (Instantiate(prefab) != null)
+            {
+                Debug.Log("Hitmarker: :)");
+            }
+
+            orig(self);
+        }
+
+        public override void OnModUnload()
+        {
+
+        }
+    }
+}
